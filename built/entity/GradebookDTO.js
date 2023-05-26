543d24fc51e6859821ae52f897bdd97b96a40c76
RobertoHuilco <133028060+RobertoHuilco@users.noreply.github.com>
2023-05-25 22:10:07 -0500

    Add files via upload

--- built/entity/GradebookDTO.js
+++ built/entity/GradebookDTO.js
@@ -1,2 +1,3 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+//Dto objeto de transferencia de datos
--- built/main.js
+++ built/main.js
@@ -82,7 +82,10 @@ function initSelect() {
     // aqui se llama al estudiante
     let assignmentsStudent = document.getElementById("assignment_student");
     document.querySelectorAll("#assignment_student option").forEach(option => option.remove());
-    students.forEach((student) => {
+    //con el forEach recorremos los datos en el archeglo
+    students.forEach(
+    // agregamos datos al archeglo studen
+    (student) => {
         let option = document.createElement("option");
         option.value = student.dni;
         option.text = student.fullName;
@@ -91,6 +94,7 @@ function initSelect() {
     // asignar la nota maxima
     let assignmentSetup = document.getElementById("assignment_gradebooksetup");
     document.querySelectorAll("#assignment_gradebooksetup option").forEach(option => option.remove());
+    // creamos un array para enviar los datos al html
     gradebookSetups.forEach((data) => {
         let option = document.createElement("option");
         option.value = data.value;
@@ -100,22 +104,40 @@ function initSelect() {
 }
 initSelect();
 // asignacion
+//de aqui se va a llevar datos a la tabla
 class Gradebook {
+    // public students: Student[];
+    // public activities: Activity[];
+    // public gradebookSetups: GradebookSetup[];
+    // public assignments: Assignment[];
+    // public teachers?: Teacher[];
     constructor(students, activities, gradebookSetups, assignments, teachers) {
         this.students = students;
         this.activities = activities;
         this.gradebookSetups = gradebookSetups;
         this.assignments = assignments;
         this.teachers = teachers;
+        //students: Student[], activities: Activity[], gradebookSetups: GradebookSetup[], assignments: Assignment[], teachers: Teacher[]
+        //     this.students = students;
+        //     this.activities = activities;
+        //     this.gradebookSetups = gradebookSetups;
+        //     this.assignments = assignments;
+        //     this.teachers = teachers;
+        // tambien se puede inisializar haciento esto en el constructor
+        // aqui se puede inisializar
     }
     ;
     buildGradebookDTOFromAssignment() {
         let gradebookDTOs = [];
         this.assignments.forEach((assignment) => {
+            //3 iguales debe tener el mismo tipo de dato
             let currentGradebooksetup = gradebookSetups.filter((item) => item.value === assignment.gradebooksetup)[0];
             let currentStudent = students.filter((student) => student.dni === assignment.student)[0];
+            //con push se inserta datos,se elimina datos con pop
+            //shut ingresamos datos y con shit se elimina datos
             let rowGradebook = {
                 //Course
+                // acedemos al atributo
                 course: currentGradebooksetup.course,
                 //Student
                 studentName: currentStudent.fullName,
@@ -124,18 +146,53 @@ class Gradebook {
                 dni: assignment.student,
                 fullName: currentStudent.fullName,
                 //GradebookSetup
-                value: "",
-                activity: "",
-                maximun_grade: 0,
+                value: currentGradebooksetup.value,
+                activity: currentGradebooksetup.activity,
+                maximun_grade: currentGradebooksetup.maximun_grade,
                 //Activity
                 name: "",
                 //Assignment
                 student: assignment.student,
                 gradebooksetup: assignment.gradebooksetup,
                 grade: assignment.grade
-            };
+            }; //agregamos al archeglo gradebookDTOs el objeto rowGradebook
             gradebookDTOs.push(rowGradebook);
+            if (assignment.grade > 70) {
+                console.log('El estudiante a pasado');
+            }
+            else {
+                console.log('El estudiante a reprobado');
+            }
         });
+        // un arcgeglo puede tener datos ,objetos, varios archeglos
         return gradebookDTOs;
     }
+}
+function generateReport() {
+    let reportGrade = new Gradebook(students, activities, gradebookSetups, assignments, teachers);
+    let rowReport = reportGrade.buildGradebookDTOFromAssignment();
+    let reportTable = document.getElementById("report");
+    rowReport.forEach((itemDTO) => {
+        let tr;
+        let td;
+        tr = reportTable.insertRow(0);
+        td = tr.insertCell(0);
+        // aqui se mapea el Html
+        td.innerHTML = itemDTO.course;
+        td = tr.insertCell(1);
+        td.innerHTML = itemDTO.studentName;
+        td = tr.insertCell(2);
+        td.innerHTML = String(itemDTO.level);
+        td = tr.insertCell(3);
+        td.innerHTML = itemDTO.activity;
+        td = tr.insertCell(4);
+        td.innerHTML = itemDTO.value;
+        td = tr.insertCell(5);
+        td.innerHTML = String(itemDTO.grade);
+        td = tr.insertCell(6);
+        td.innerHTML = String(itemDTO.dni);
+        td = tr.insertCell(7);
+        td.innerHTML = String(itemDTO.maximun_grade);
+        td = tr.insertCell(8);
+    });
 }
